a79017666090f3ea8a434491a6a2d3667cf1f636
miuoshv <miuoshv@gmail.com>
2022-06-06 21:38:01 +0200

    nauka

---
+++ app.js
@@ -0,0 +1,35 @@
+const sizeUp = document.querySelector('.sizeUp')
+const sizeDown = document.querySelector('.sizeDown')
+const color = document.querySelector('.color')
+const text = document.querySelector('p')
+let size = 36
+
+
+
+
+
+const sizeUpFun = () => {
+  if (size < 40) {
+    size = size + 1
+  }
+  text.style.fontSize = size + 'px'
+}
+
+const sizeDownFun = () => {
+  if (size > 28){
+    size = size - 1
+  }
+  text.style.fontSize = size + 'px'
+}
+
+const colorFun = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16)
+  text.style.color = '#' + randomColor
+}
+
+
+sizeUp.addEventListener('click', sizeUpFun)
+
+sizeDown.addEventListener('click', sizeDownFun)
+
+color.addEventListener('click', colorFun)
